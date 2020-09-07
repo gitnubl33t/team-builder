8cf950577c5298cc67a6e26c8026620f14c42010
gitnubl33t <jessicad8695@gmail.com>
2020-09-07 10:32:30 -0700

    Created Teamform function with usestate

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import "./App.css";
+import TeamForm from "./Components/TeamForm";
 
 function App() {
+  const [memberList, setMemberList] = useState([
+    {
+      id: 1,
+      name: "Jon Snow",
+      email: "jonsnow@winterfell.com",
+      role: "Commander of the Night's Watch",
+    },
+  ]);
+
+  const addNewMember = (formData) => {
+    const newMember = {
+      id: Date.now(),
+      name: formData.name,
+      email: formData.email,
+      role: formData.role,
+    };
+    setMemberList([...memberList, newMember]);
+  };
+
   return (
     <div className="App">
-      <h1>This is an H1</h1>
+      <h1>Team Member List</h1>
+      <TeamForm addNewMember={addNewMember} />
     </div>
   );
 }
---
+++ team-builder/src/Components/TeamForm.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+const TeamForm = (props) => {
+  console.log("TeamForm props: ", props);
+  const [teamMember, setMember] = useState({
+    name: "",
+    email: "",
+    role: "",
+  });
+
+  const changeHandler = (e) => {
+    console.log(e.target.value);
+    console.log("teamMember: ", teamMember);
+    setMember({
+      ...teamMember,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+    props.addNewMember(teamMember);
+    setMember({ name: "", email: "", role: "" });
+  };
+
+  return (
+    <form onSumbit={submitForm}>
+      <label htmlFor="name">Name</label>
+      <input
+        type="text"
+        name="name"
+        value={teamMember.name}
+        onChange={changeHandler}
+      />
+      <label htmlFor="email">Email</label>
+      <input
+        type="text"
+        name="email"
+        value={teamMember.email}
+        onChange={changeHandler}
+      />
+
+      <label htmlFor="role">Role</label>
+      <input
+        type="text"
+        name="role"
+        value={teamMember.role}
+        onChange={changeHandler}
+      />
+
+      <button type="submit">Add New Member</button>
+    </form>
+  );
+};
+
+export default TeamForm;
